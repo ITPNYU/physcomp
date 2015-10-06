1596f0a9f6eb96b2107cf6108a166e1ac29cf472
tigoe <tom.igoe@gmail.com>
2015-10-06 16:13:28 -0400

    updated P5sendSerial lab

--- labs2015/P5SendSerial/PhysicalPixel2/PhysicalPixel2.ino
+++ labs2015/P5SendSerial/PhysicalPixel2/PhysicalPixel2.ino
@@ -1,26 +1,8 @@
 /*
-  Physical Pixel
-
- An example of using the Arduino board to receive data from the
- computer.  In this case, the Arduino boards turns on an LED when
- it receives the character 'H', and turns off the LED when it
- receives the character 'L'.
-
- The data can be sent from the Arduino serial monitor, or another
- program like Processing (see code below), Flash (via a serial-net
- proxy), PD, or Max/MSP.
-
- The circuit:
- * LED connected from digital pin 13 to ground
-
  created 2006
  by David A. Mellis
- modified 30 Aug 2011
- by Tom Igoe and Scott Fitzgerald
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/PhysicalPixel
+ modified 4 Oct 2015
+ by Tom Igoe 
  */
 
 const int ledPin = 5; // the pin that the LED is attached to
