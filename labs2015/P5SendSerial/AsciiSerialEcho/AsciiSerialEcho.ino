9ba05c7c5901b693a40b7166ce4c47bd2c815499
tigoe <tom.igoe@gmail.com>
2018-08-15 16:58:39 -0400

    updated p5 serial examples to include tone

--- labs2015/P5SendSerial/AsciiSerialEcho/AsciiSerialEcho.ino
+++ labs2015/P5SendSerial/AsciiSerialEcho/AsciiSerialEcho.ino
@@ -1,14 +1,16 @@
-  void setup() {
-    Serial.begin(9600);     // initialize serial communications
-    Serial.setTimeout(10);  // set the timeout for parseInt
-  }
-  
-  void loop() {
-    if (Serial.available() > 0) {     // if there's serial data available
-      int inByte = Serial.parseInt(); // read it
-      if (inByte > 0) { 
-        Serial.write(inByte);         // send it back out as raw binary data
-        analogWrite(5, inByte);       // use it to set the LED brightness
-      }
+void setup() {
+  Serial.begin(9600);     // initialize serial communications
+  Serial.setTimeout(10);  // set the timeout for parseInt
+}
+
+void loop() {
+  if (Serial.available() > 0) {     // if there's serial data available
+    int inByte = Serial.parseInt(); // read it
+    if (inByte > 0) {
+      Serial.write(inByte);         // send it back out as raw binary data
+      analogWrite(5, inByte);       // use it to set the LED brightness
+      // if you're using a speaker instead of an LED, uncomment line below  and comment out the previous line:
+      //  tone(5, inByte*10);     // play tone on pin 5
     }
   }
+}
--- labs2015/P5SendSerial/serialEcho/serialEcho.ino
+++ labs2015/P5SendSerial/serialEcho/serialEcho.ino
@@ -7,5 +7,7 @@ void loop() {
     int inByte = Serial.read(); // read it
     Serial.write(inByte);       // send it back out as raw binary data
     analogWrite(5, inByte);     // use it to set the LED brightness
+    // if you're using a speaker instead of an LED, uncomment line below  and comment out the previous line:
+    //  tone(5, inByte*10);     // play tone on pin 5
   }
 }
