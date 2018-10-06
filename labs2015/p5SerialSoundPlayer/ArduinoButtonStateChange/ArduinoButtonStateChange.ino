38226adec55fad3ec7a58056c0eb632e38e5dca7
tigoe <tom.igoe@gmail.com>
2018-10-06 17:04:15 -0400

    changed buttonState example for p5 sound player

--- labs2015/p5SerialSoundPlayer/ArduinoButtonStateChange/ArduinoButtonStateChange.ino
+++ labs2015/p5SerialSoundPlayer/ArduinoButtonStateChange/ArduinoButtonStateChange.ino
@@ -1,4 +1,4 @@
-const int buttonPin = 2;     // pin that the button's connected to
+const int buttonPin = 5;     // pin that the button's connected to
 int lastButtonState = 0;     // previous state of the button
 
 void setup() {
@@ -12,24 +12,12 @@ void setup() {
 void loop() {
   // read the pushbutton input pin:
   int buttonState = digitalRead(buttonPin);
-
   // compare the buttonState to its previous state
   if (buttonState != lastButtonState) {
-    // if the state has changed, and it's now HIGH
-    if (buttonState == HIGH) {
+    // if the state has changed,
       Serial.println(buttonState);    // print it
-    }
   }
   // save the current state as the last state,
   //for next time through the loop
   lastButtonState = buttonState;
 }
-
-
-
-
-
-
-
-
-
