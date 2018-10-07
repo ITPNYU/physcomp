ab6cd8cad1bd3f26173c6c1f4a287b376a79eb6a
tigoe <tom.igoe@gmail.com>
2018-10-07 14:03:09 -0400

    Updated p5 listPorts sketch

--- labs2015/p5ListPorts/sketch.js
+++ labs2015/p5ListPorts/sketch.js
@@ -8,28 +8,40 @@ and displays any incoming strings as text onscreen.
 Works with P5 editor as the serial server, version 0.5.5 or later.
 
 created 2 Oct 2015
+modified 7 Oct 2018
 by Tom Igoe
 */
 
-var serial; // Declare a "SerialPort" object
-var menu;
-var result = '';
+var serial;       // Declare a "SerialPort" object
+var menu;         // a variable to hold the options menu
+var textDiv;      // a variable to hold the text div for incoming data
+var result = '';  // a variable for the incoming serial data
 
 function setup() {
-  createCanvas(400, 300); // window size
+  noCanvas();
+  createHTML();
   serial = new p5.SerialPort();
-  serial.on('list', printList);
+  serial.on('list', printList); 
   serial.on('data', printData);
-  serial.list();
 }
-
 
 function draw() {
   background(255);
   fill(0);
-  text(result, 10, 60);
 }
 
+function createHTML() {
+  // make an option menu for the serial ports:
+  menu = createSelect();
+  menu.option("Choose a serial port: ");
+  menu.attribute('aria-label', 'Serial port selector');
+  menu.position(10, 10);
+  menu.changed(openPort);
+// make a DOM element to take the incoming serial data:
+  dataLabel = createElement('section', 'Incoming Serial Data');
+  dataLabel.attribute('aria-label', 'serial input');
+  dataLabel.position(10, 60);
+}
 function openPort() {
   portName = menu.elt.value;
   serial.open(portName);
@@ -40,19 +52,12 @@ function printData() {
   trim(inString);
   if (!inString) return;
   result = inString;
+  dataLabel.html(result);
 }
 
 // Got the list of ports
 function printList(serialList) {
-  menu = createSelect();
-  var title = createElement('option', 'Choose a port:');
-  menu.child(title);
-  menu.position(10, 10);
-  menu.changed(openPort);
   for (var i = 0; i < serialList.length; i++) {
-    var thisOption = createElement('option', serialList[i]);
-    thisOption.value = serialList[i];
-    menu.child(thisOption);
-    console.log(i + " " + serialList[i]);
+      menu.option(serialList[i]);
   }
 }
