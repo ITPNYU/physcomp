e9d5bc8961917c48aaca190deb823dc8ca32230f
tigoe <tom.igoe@gmail.com>
2019-09-01 11:02:32 -0400

    Update PeripheralDuplex.ino

--- Labs/LabBluetoothLE/BLEDuplex/PeripheralDuplex/PeripheralDuplex.ino
+++ Labs/LabBluetoothLE/BLEDuplex/PeripheralDuplex/PeripheralDuplex.ino
@@ -88,7 +88,7 @@ void loop() {
         Serial.println("changing LED");
       }
     }
-    
+
     // when the central disconnects, print it out:
     Serial.print(F("Disconnected from central: "));
     Serial.println(central.address());
