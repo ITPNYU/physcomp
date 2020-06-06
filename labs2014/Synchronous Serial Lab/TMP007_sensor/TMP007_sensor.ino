6420a94558b3bfdf63de6b6f7704abe3f4c3ce00
tigoe <tom.igoe@gmail.com>
2020-06-06 09:41:34 -0400

    Update TMP007_sensor.ino

--- labs2014/Synchronous Serial Lab/TMP007_sensor/TMP007_sensor.ino
+++ labs2014/Synchronous Serial Lab/TMP007_sensor/TMP007_sensor.ino
@@ -4,27 +4,27 @@
   Some names have been changed for readability.
 
 */
-
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include "Adafruit_TMP007.h"
+//#include <Adafruit_Sensor.h>
+#include <Adafruit_TMP007.h>
 
-Adafruit_TMP007 tmp007;    // instance of the sensor library
+Adafruit_TMP007 tmp007; // instance of the sensor library
 
 void setup() {
   Serial.begin(9600);
-  //boolean sensorInitialized = tmp007.begin();  // initialize the sensor
-  
-  while (!tmp007.begin()) {    // Do nothing until sensor responds
+  boolean sensorInitialized = tmp007.begin(); // initialize the sensor
+
+  while (!sensorInitialized) {
+    // Do nothing until sensor responds
     Serial.println("Sensor is not responding");
   }
 }
 
 void loop() {
-  if (millis() % 4000 < 2) {    // if 4 seconds have passed
-    float objectTemp = tmp007.readObjTempC();    // read object temperature
-    float chipTemp = tmp007.readDieTempC();      // read chip temperature
-    
+  if (millis() % 4000 < 2) { // if 4 seconds have passed
+    float objectTemp = tmp007.readObjTempC(); // read object temperature
+    float chipTemp = tmp007.readDieTempC(); // read chip temperature
+
     // print the results:
     Serial.print("Chip temperature: ");
     Serial.print(chipTemp);
