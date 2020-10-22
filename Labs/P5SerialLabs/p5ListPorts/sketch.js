fecc184f4faecb7b8cbde626aa2581b8f53dc6cd
Tom Igoe <tom.igoe@gmail.com>
2020-10-22 15:24:32 -0400

    modified p5 listPorts

--- Labs/P5SerialLabs/p5ListPorts/sketch.js
+++ Labs/P5SerialLabs/p5ListPorts/sketch.js
@@ -8,83 +8,64 @@ it accessible to screen readers.
 Works with P5 editor as the serial server, version 0.5.5 or later.
 
 created 2 Oct 2015
-modified 8 Oct 2018
+modified 20 Oct 2020
 by Tom Igoe
 */
+/
 
-var serial;       // Declare a "SerialPort" object
-var menu;         // a variable to hold the options menu
-var result = '';  // a variable for the incoming serial data
-var serialInput;  // a div for incoming serial data
-var hidden = false; // whether the serial controls are hidden or not
+// variable to hold an instance of the serialport library:
+let serial;
+// HTML Select option object:
+let portSelector;
 
 function setup() {
-  // create some HTML elements to manage serial input:
-  createHTML();
-  // open an instance of serialport and set up event handlers:
-  serial = new p5.SerialPort();
-  serial.on('list', printList);
-  serial.on('data', printData);
+  serial = new p5.SerialPort(); // new instance of the serialport library
+  serial.on('list', printList); // callback function for serialport list event
+  serial.on('data', serialEvent); // callback function for serialport data event
+  serial.list(); // list the serial ports
 }
 
-// nothing happens in the draw function, since it's all 
-// based on serial input and keyboard input:
-function draw() {
-
-}
-
-function createHTML() {
-  // make an option menu for the serial ports:
-  menu = createSelect();
-  menu.option("Choose a serial port: ");
-  menu.attribute('aria-label', 'Serial port selector');
-  menu.position(10, 30);
-  menu.changed(openPort);
-  // get the DOM element for showing incoming serial data:
-  serialInput = document.getElementById('serial');
-  // convert it to a p5.elt so we can use the p5 DOM functions on it:
-  serialInput = new p5.Element(serialInput);
-}
-
-function openPort() {
-  // get the value of the option chosen from the select menu:
-  portName = menu.elt.value;
-  // open the port:
-  serial.open(portName);
-  // notify the user in the HTML div:
-  serialInput.html('Serial port ' + portName + ' is open.')
-}
-
-function printData() {
-  // read serial input:
-  var inString = serial.readStringUntil('\r\n');
-  // trim any whitespace:
-  trim(inString);
-  // if there's no string, quit this function:
-  if (!inString) return;
-  // put the result in the serialInput div:
-  serialInput.html(inString);
-  console.log(inString);
-}
-
-// Got the list of ports
-function printList(serialList) {
-  // add serial port list items to the options menu:
-  for (var i = 0; i < serialList.length; i++) {
-    menu.option(serialList[i]);
+// make a serial port selector object:
+function printList(portList) {
+  // create a select object:
+  portSelector = createSelect();
+  portSelector.position(10, 10);
+  // portList is an array of serial port names
+  for (var i = 0; i < portList.length; i++) {
+    // add this port name to the select object:
+    portSelector.option(portList[i]);
   }
+  // set an event listener for when the port is changed:
+  portSelector.changed(mySelectEvent);
 }
 
-function keyReleased() {
-  // if the spacebar is pressed, show or hide the serial menu and display div
-  if (key == ' ') {
-    if (!hidden) {
-      serialInput.hide();
-      menu.hide();
+
+function mySelectEvent() {
+  let item = portSelector.value();
+  // give it an extra property for hiding later:
+  portSelector.visible = true;
+  // if there's a port open, close it:
+  if (serial.serialport != null) {
+    serial.close();
+  }
+  // open the new port:
+  serial.open(item);
+}
+
+function keyPressed() {
+  // if port selector is visible hide, else show:
+  if (portSelector) {
+    if (portSelector.visible) {
+      portSelector.hide();
+      portSelector.visible = false;
     } else {
-      serialInput.show();
-      menu.show();
+      portSelector.show();
+      portSelector.visible = true;
     }
-    hidden = !hidden;
   }
+}
+
+function serialEvent() {
+  var inData = serial.readLine();
+  console.log(inData);
 }
