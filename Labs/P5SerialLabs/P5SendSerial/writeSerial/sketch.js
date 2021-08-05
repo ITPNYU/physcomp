53bd50aa723cc864df25324d361359f2b2fef1f6
Tom Igoe <tom.igoe@gmail.com>
2021-08-05 10:09:04 -0400

    updates to p5 serial labs

--- Labs/P5SerialLabs/P5SendSerial/writeSerial/sketch.js
+++ Labs/P5SerialLabs/P5SendSerial/writeSerial/sketch.js
@@ -2,13 +2,16 @@ var serial;                            // variable to hold an instance of the se
 var portName = '/dev/cu.usbmodem14121'; // fill in your serial port name here
 var inData;                            // for incoming serial data
 var outByte = 0;                       // for outgoing data
+var portSelector;
 
 function setup() {
   createCanvas(400, 300);          // make the canvas
   serial = new p5.SerialPort();    // make a new instance of the serialport library
   serial.on('data', serialEvent);  // callback for when new data arrives
   serial.on('error', serialError); // callback for errors
-  serial.open(portName);           // open a serial port
+  serial.on('list', printList);       // set a callback function for the serialport list event
+  serial.list();                   // list the serial ports
+  // serial.open(portName);           // open a serial port
 }
 
 function draw() {
@@ -16,7 +19,7 @@ function draw() {
   background(0);
   fill(255);
   // display the incoming serial data as a string:
-  text("incoming value: " + inData, 30, 30);
+  text("incoming value: " + inData, 30, 50);
 }
 
 function mouseDragged() {
@@ -42,4 +45,29 @@ function serialEvent() {
 
 function serialError(err) {
   console.log('Something went wrong with the serial port. ' + err);
+}
+
+
+// make a serial port selector object:
+function printList(portList) {
+  // create a select object:
+  portSelector = createSelect();
+  portSelector.position(10, 10);
+  // portList is an array of serial port names
+  for (var i = 0; i < portList.length; i++) {
+    // add this port name to the select object:
+    portSelector.option(portList[i]);
+  }
+  // set an event listener for when the port is changed:
+  portSelector.changed(mySelectEvent);
+}
+
+function mySelectEvent() {
+  let item = portSelector.value();
+  // if there's a port open, close it:
+  if (serial.serialport != null) {
+    serial.close();
+  }
+  // open the new port:
+  serial.open(item);
 }
