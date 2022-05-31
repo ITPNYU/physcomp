db573c33cfe45ae01b45a5a22f8dc2202abbc448
Tom Igoe <tom.igoe@gmail.com>
2022-05-31 16:14:34 -0400

    Added p5SendSerialWebSerial example

---
+++ Labs/P5SerialLabs/P5SendSerial/AsciiWriteSerialWebSerial/sketch.js
@@ -0,0 +1,124 @@
+/*
+Serial write using p5.webserial
+
+Sends ASCII numeric strings out on mouse drag or
+key press 'H' or 'L'. Reads bytes in and interprets
+them as binary values
+
+created 31 May 2022
+by Tom Igoe
+*/
+// variable to hold an instance of the p5.webserial library:
+const serial = new p5.WebSerial();
+
+// HTML button object:
+let portButton;
+let inData;                            // for incoming serial data
+let outByte = 0;                       // for outgoing data
+
+function setup() {
+  createCanvas(400, 300);          // make the canvas
+   // check to see if serial is available:
+   if (!navigator.serial) {
+    alert("WebSerial is not supported.");
+  }
+  // if serial is available, add connect/disconnect listeners:
+  navigator.serial.addEventListener("connect", portConnect);
+  navigator.serial.addEventListener("disconnect", portDisconnect);
+  // check for any ports that are available:
+  serial.getPorts();
+  // if there's no port chosen, choose one:
+  serial.on("noport", makePortButton);
+  // open whatever port is available:
+  serial.on("portavailable", openPort);
+  // handle serial errors:
+  serial.on("requesterror", portError);
+  // handle any incoming serial data:
+  serial.on("data", serialEvent);
+}
+function draw() {
+  // black background, white text:
+  background(0);
+  fill(255);
+  // display the incoming serial data as a string:
+  text("incoming value: " + inData, 30, 30);
+}
+
+// sends the Y value out as an ascii-numeric
+//  string. works with the Arduino sketch called 
+// AsciiSerialEcho:
+function mouseDragged() {
+  // map the mouseY to a range from 0 to 255:
+  outByte = int(map(mouseY, 0, height, 0, 255));
+  // Convert it to a string with a newline at the end,
+  // and send it out the serial port:
+  serial.write(outByte + '\n');
+}
+
+// sends an H or L if you press those keys.
+// works with the Arduino sketch called
+// PhysicalPixel or PhysicalPixel2:
+function keyPressed() {
+  if (key ==='H' || key ==='L') {   // if the user presses H or L
+    serial.write(key);      // send it out the serial port
+    console.log(key);
+  }
+}
+
+function serialEvent() {
+  // read a byte from the serial port:
+  var inByte = serial.read();
+  // store it in a global variable:
+  inData = inByte;
+}
+
+// if there's no port selected, 
+// make a port select button appear:
+function makePortButton() {
+  // create and position a port chooser button:
+  portButton = createButton('choose port');
+  portButton.position(10, 10);
+  // give the port button a mousepressed handler:
+  portButton.mousePressed(choosePort);
+}
+
+// make the port selector window appear:
+function choosePort() {
+  serial.requestPort();
+}
+
+// open the selected port, and make the port 
+// button invisible:
+function openPort() {
+  serial.open();
+  console.log("port open")
+  // hide the port button once a port is chosen:
+  if (portButton) portButton.hide();
+}
+
+// read any incoming data as a byte:
+function serialEvent() {
+  // read a byte from the serial port:
+  var inByte = serial.read();
+  // store it in a global variable:
+  inData = inByte;
+}
+
+// pop up an alert if there's a port error:
+function portError(err) {
+  alert("Serial port error: " + err);
+}
+
+// try to connect if a new serial port 
+// gets added (i.e. plugged in via USB):
+function portConnect() {
+  console.log("port connected");
+  serial.getPorts();
+}
+
+// if a port is disconnected:
+function portDisconnect() {
+  serial.close();
+  console.log("port disconnected");
+}
+
