50c8e8aa626a47efb90c19d2e2b66ef80a0c400c
Tom Igoe <tom.igoe@gmail.com>
2022-05-31 13:02:03 -0400

    added webSerial equivalents to the p5serial labs

---
+++ Labs/P5SerialLabs/P5ReadSerial/AsciiGraphSerialWebSerial/sketch.js
@@ -0,0 +1,126 @@
+/*
+Serial read data and graph using p5.webserial
+Listens for an ASCII string with a newline at the end.
+
+Lists serial ports in a browser selection window. 
+When you choose a port, it opens the port and 
+graphs the incoming data.
+
+created 31 May 2022
+by Tom Igoe
+*/
+
+// variable to hold an instance of the p5.webserial library:
+const serial = new p5.WebSerial();
+
+// HTML button object:
+let portButton;
+let inData = 0;          // for incoming serial data
+let xPos = 0;            // x position of the graph
+
+function setup() {
+  createCanvas(400, 300);
+  background(0x08, 0x16, 0x40);
+
+   // check to see if serial is available:
+   if (!navigator.serial) {
+    alert("WebSerial is not supported.");
+  }
+  // if serial is available, add connect/disconnect listeners:
+  navigator.serial.addEventListener("connect", portConnect);
+  navigator.serial.addEventListener("disconnect", portDisconnect);
+  // check for any ports that are available:
+  serial.getPorts();
+  // if there's no port chosen, choose one:
+  serial.on("noport", makePortButton);
+  // open whatever port is available:
+  serial.on("portavailable", openPort);
+  // handle serial errors:
+  serial.on("requesterror", portError);
+  // handle any incoming serial data:
+  serial.on("data", serialEvent);
+}
+
+function draw() {
+   graphData(inData);
+}
+
+function serialError(err) {
+  console.log('Something went wrong with the serial port. ' + err);
+}
+
+function portClose() {
+  console.log('The serial port closed.');
+}
+
+function graphData(newData) {
+  // map the range of the input to the window height:
+  var yPos = map(newData, 0, 255, 0, height);
+  // draw the line in a pretty color:
+  stroke(0xA8, 0xD9, 0xA7);
+  line(xPos, height, xPos, height - yPos);
+  // at the edge of the screen, go back to the beginning:
+  if (xPos >= width) {
+    xPos = 0;
+    // clear the screen by resetting the background:
+    background(0x08, 0x16, 0x40);
+  } else {
+    // increment the horizontal position for the next reading:
+    xPos++;
+  }
+}
+
+
+// if there's no port selected, 
+// make a port select button appear:
+function makePortButton() {
+  // create and position a port chooser button:
+  portButton = createButton('choose port');
+  portButton.position(10, 10);
+  // give the port button a mousepressed handler:
+  portButton.mousePressed(choosePort);
+}
+
+// make the port selector window appear:
+function choosePort() {
+  serial.requestPort();
+}
+
+// open the selected port, and make the port 
+// button invisible:
+function openPort() {
+  serial.open();
+  console.log("port open")
+  // hide the port button once a port is chosen:
+  if (portButton) portButton.hide();
+}
+
+// read incoming bytes as strings:
+function serialEvent() {
+  // read a string from the serial port:
+  let inString = serial.readStringUntil('\r\n');
+  // check to see that there's actually a string there:
+  if (inString.length > 0 ) {
+  // convert it to a number:
+  inData = Number(inString);
+  }
+}
+
+
+// pop up an alert if there's a port error:
+function portError(err) {
+  alert("Serial port error: " + err);
+}
+
+// try to connect if a new serial port 
+// gets added (i.e. plugged in via USB):
+function portConnect() {
+  console.log("port connected");
+  serial.getPorts();
+}
+
+// if a port is disconnected:
+function portDisconnect() {
+  serial.close();
+  console.log("port disconnected");
+}
