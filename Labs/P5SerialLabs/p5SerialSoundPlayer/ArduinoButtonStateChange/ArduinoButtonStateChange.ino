0af560cf2f8377ad0d74eb933c357dcc766384d8
Tom Igoe <tom.igoe@gmail.com>
2022-06-12 13:55:02 -0400

    Update ArduinoButtonStateChange.ino

--- Labs/P5SerialLabs/p5SerialSoundPlayer/ArduinoButtonStateChange/ArduinoButtonStateChange.ino
+++ Labs/P5SerialLabs/p5SerialSoundPlayer/ArduinoButtonStateChange/ArduinoButtonStateChange.ino
@@ -1,30 +1,24 @@
 const int buttonPin = 5; // pin that the button's connected to
 int lastButtonState = 0; // previous state of the button
 
-void setup()
-{
+void setup() {
   // initialize the button pin as an input:
   pinMode(buttonPin, INPUT);
   // initialize serial communication:
   Serial.begin(9600);
 }
 
-void loop()
-{
+void loop() {
   // read the pushbutton input pin:
   int buttonState = digitalRead(buttonPin);
   // compare the buttonState to its previous state
-  if (buttonState != lastButtonState)
-  {
+  if (buttonState != lastButtonState) {
     // if the state has changed,
     delay(3); // debounce delay
-    if (buttonState == HIGH)
-    {
+    if (buttonState == HIGH) {
       // send A
       Serial.println("1"); // print it
-    }
-    else
-    {
+    } else {
       // if it's low, send L
       Serial.println("0");
     }
